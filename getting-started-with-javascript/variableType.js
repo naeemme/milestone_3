d33c376952b96fc044c51be1ee991b9309a5dfd4
naeemme <abun1347@gmail.com>
2021-09-02 23:50:31 +0600

    variable

---
+++ getting-started-with-javascript/variableType.js
@@ -0,0 +1,55 @@
+var applePrice = 15;
+console.log(typeof applePrice);
+
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = 'naeem';
+console.log(typeof name);
+
+var applePrice = 15;
+console.log(applePrice);
+
+var seenAfter = 21;
+console.log(seenAfter);
+
+var name = 'naeem';
+console.log(name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+//class practice//
+
+var tourCost = 1500;
+console.log(tourCost);
+
+var tourCost = 1500;
+console.log(typeof tourCost);
+
+var hotelCost = 1200;
+console.log(hotelCost);
+
+var hotelCost = 1200;
+console.log(typeof hotelCost);
+
+var location = 'mymensing';
+console.log(location);
+
+var location = 'mymensing';
+console.log(typeof location);
+
+var laptop = 'asus';
+console.log(laptop);
+
+var laptop = 'asus';
+console.log(typeof laptop);
+
+var ishonest = true;
+var isfrod = false;
+console.log(ishonest);
+
+var ishonest = true;
+var isfrod = false;
+console.log(typeof ishonest);
