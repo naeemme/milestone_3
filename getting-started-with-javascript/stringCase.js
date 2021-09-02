48c9cb408860ce8ad4c8d0050c30083ee44d780f
naeemme <abun1347@gmail.com>
2021-09-03 00:36:41 +0600

    string

---
+++ getting-started-with-javascript/stringCase.js
@@ -0,0 +1,11 @@
+var promise = 'i promise i will work hard to become a programmer';
+console.log(promise.toLowerCase());
+
+var promise = 'i promise i will work hard to become a programmer';
+console.log(promise.toUpperCase());
+
+var promise = 'i promise i will work hard to become a programmer';
+console.log(promise.indexOf('will'));
+
+var promise = 'i promise i will work hard to become a programmer';
+console.log(promise.split(' '));
